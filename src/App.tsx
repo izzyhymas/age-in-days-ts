979d37d8d45f2eb3242ac0c6523081529e26aa59
Izzy Hymas <izzyhymas@gmail.com>
2024-07-16 11:24:11 -0600

    Create Calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
 
-function App() {
+import Calculator from "./Calculator";
 
+function App() {
   return (
     <>
+      <header>
+        <h1>Age in Days Calculator</h1>
+        <p>
+          Given your age, this app will tell you how many days you've lived.
+        </p>
+      </header>
+      <main>
+        <Calculator />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
