4273e09f951e620bf54b4eb32c6ba576121ab4a8
Izzy Hymas <izzyhymas@gmail.com>
2024-07-17 11:07:06 -0600

    Create Age label

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
-    return (
-        <></>
-    );
-}
+  const [name, setName] = useState("");
+  const [age, setAge] = useState<number | string>();
+
+  const handleAgeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setAge(event.target.valueAsNumber);
+    // const newAge = event.target.value;
+    // if (newAge === "") {
+    //   setAge("");
+    // } else {
+    //   setAge(Number(newAge));
+    // }
+  };
+
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            placeholder="Enter Name"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+              setName(event.target.value)
+            }
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            value={age}
+            placeholder="Enter Age"
+            onChange={handleAgeChange}
+          ></input>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default Calculator;
